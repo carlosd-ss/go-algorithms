06f751e35e3b6abfdf95e1397d49e85008b9e7ac
Carlos Daniel <carlos-dss@hotmail.com.br>
2021-01-13 14:31:33 -0300

    add the go

---
+++ the-go-programming-language/2-program-structure/ex1/tempconv/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/carlosd-ss/go-algorithms/the-go-programming-language/2-program-structure/ex1/tempconv"
+)
+
+func main() {
+	fmt.Println(tempconv.Ctof(tempconv.BoilingC))
+}
